4fdbd7a3210b78ec5edae63699f63b445d675c51
Marin Kaçaj <m93_vkacaj@yahoo.com>
2018-12-30 15:52:56 +0100

    Set proper names for company, site, etc

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -9,20 +9,20 @@ const Privacy = () => (
       Generic Privacy Policy template
 
 Privacy Policy
-Last updated: (add date)
-My Company (change this) ("us", "we", or "our") operates http://www.mysite.com (change this) (the
-"Site"). This page informs you of our policies regarding the collection, use and disclosure of
+Last updated: \(30th of December 2018\)
+Tanci Craft \(\"us\", \"we\", or \"our\"\) operates http://tanci-craft-dev.netlify.com \(the
+\"Site\"\). This page informs you of our policies regarding the collection, use and disclosure of
 Personal Information we receive from users of the Site.
 We use your Personal Information only for providing and improving the Site. By using the Site, you
 agree to the collection and use of information in accordance with this policy.
 Information Collection And Use
 While using our Site, we may ask you to provide us with certain personally identifiable information
 that can be used to contact or identify you. Personally identifiable information may include, but is not
-limited to your name ("Personal Information").
+limited to your name \(\"Personal Information\"\).
 Log Data
 Like many site operators, we collect information that your browser sends whenever you visit our Site
-("Log Data").
-This Log Data may include information such as your computer's Internet Protocol ("IP") address,
+\(\"Log Data\"\).
+This Log Data may include information such as your computer\'s Internet Protocol \(\"IP\"\) address,
 browser type, browser version, the pages of our Site that you visit, the time and date of your visit,
 the time spent on those pages and other statistics.
 In addition, we may use third party services such as Google Analytics that collect, monitor and
@@ -38,8 +38,8 @@ newsletters) or other methods. For the full disclosure section, create your own 
 
 Cookies
 Cookies are files with small amount of data, which may include an anonymous unique identifier.
-Cookies are sent to your browser from a web site and stored on your computer's hard drive.
-Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all
+Cookies are sent to your browser from a web site and stored on your computer\'s hard drive.
+Like many sites, we use \"cookies\" to collect information. You can instruct your browser to refuse all
 cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may
 not be able to use some portions of our Site.
 Security
@@ -48,7 +48,7 @@ transmission over the Internet, or method of electronic storage, is 100% secure.
 use commercially acceptable means to protect your Personal Information, we cannot guarantee its
 absolute security.
 Changes To This Privacy Policy
-This Privacy Policy is effective as of (add date) and will remain in effect except with respect to any
+This Privacy Policy is effective as of 30th of December 2018 and will remain in effect except with respect to any
 changes in its provisions in the future, which will be in effect immediately after being posted on this
 page.
 We reserve the right to update or change our Privacy Policy at any time and you should check this
