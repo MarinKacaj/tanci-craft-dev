b10f9ebb5e671b6cd55e3c8232acdd0933827e94
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-01-01 18:37:24 +0100

    Update terms.js

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -6,7 +6,43 @@ const Terms = () => (
     <Helmet title="Terms" />
     <h1>Terms</h1>
     <h2>Welcome to Tanci Craft</h2>
-      </div>
+    <p>
+      These terms and conditions outline the rules and regulations for the use
+      of Tanci Craft's Website.
+    </p>
+    <br />
+    <span style="text-transform: capitalize;">Tanci Craft</span> is located at:
+    <br />
+    <address>
+      Rruga Endri Keko Tirana, Tirana
+      <br />
+      Albania - 1005, Albania
+      <br />
+    </address>
+    <p>
+      By accessing this website we assume you accept these terms and conditions
+      in full. Do not continue to use Tanci Craft's website if you do not accept
+      all of the terms and conditions stated on this page.
+    </p>
+    <p>
+      The following terminology applies to these Terms and Conditions, Privacy
+      Statement and Disclaimer Notice and any or all Agreements: “Client”, “You”
+      and “Your” refers to you, the person accessing this website and accepting
+      the Company’s terms and conditions. “The Company”, “Ourselves”, “We”,
+      “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers
+      to both the Client and ourselves, or either the Client or ourselves. All
+      terms refer to the offer, acceptance and consideration of payment
+      necessary to undertake the process of our assistance to the Client in the
+      most appropriate manner, whether by formal meetings of a fixed duration,
+      or any other means, for the express purpose of meeting the Client’s needs
+      in respect of provision of the Company’s stated services/products, in
+      accordance with and subject to, prevailing law of Albania. Any use of the
+      above terminology or other words in the singular, plural, capitalisation
+      and/or he/she or they, are taken as interchangeable and therefore as
+      referring to same.
+    </p>
+    
+  </div>
 );
 
 export default Terms;
