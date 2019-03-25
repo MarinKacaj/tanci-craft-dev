f4aacdba2f680ce470b77b4808c5461d3cc2d1df
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-03-25 17:43:26 +0100

    added fb open graph meta tags

--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -30,6 +30,11 @@ class ProductPageTemplate extends React.PureComponent {
         <Helmet>
           <title>{title}</title>
           <meta name="description" content={description} />
+          <meta property="og:url" content={props.location.pathname} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={image} />
+          <meta property="og:image:alt" content={title} />
         </Helmet>
         <section>
           <ProductSummary {...product} />
