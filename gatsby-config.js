7e9b8d6ffed39abf7570ce2bb9e3677c9a628034
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-04-07 19:36:05 +0200

    inserted tracking uid into gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-137861858-1`,
       },
     },
     {
