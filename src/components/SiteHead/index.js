8a3f5663830b34db5de317ad7ba27308047908f1
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-04-15 22:49:29 +0200

    move I <3 handmade to separate component [skip ci]

---
+++ src/components/SiteHead/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Image, Header } from 'semantic-ui-react'
+import logo from '../../images/heart-red.png'
+
+class SiteHeader extends React.PureComponent {
+  render() {
+    return (
+      <Header as="h3" icon textAlign="center" style={{ marginBottom: '2em' }}>
+        <Header.Content style={{ width: '100%', margin: '0 auto' }}>
+          <p style={{ fontSize: '5vw', letterSpacing: '0.3em' }}>
+            I <Image style={{ display: 'inline', height: '1em', verticalAlign: 'text-bottom' }} src={logo} alt={'logo'} /> HANDMADE
+          </p>
+        </Header.Content>
+      </Header>
+    )
+  }
+}
+
+export default SiteHeader
