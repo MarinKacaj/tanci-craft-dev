9a1fb0e620030664c12989bc3a9baf501082d784
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-05-12 19:27:53 +0200

    Update contact-thanks.js

--- src/pages/contact-thanks.js
+++ src/pages/contact-thanks.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Header, Container } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
 export default class ContactThanks extends React.Component {
   render() {
     return (
-      <Containter text>
+      <div>
         <Helmet title="Contact us" />
         <Header as="h1">Message sent successfully</Header>
         <p>Thank you! Your message has been received. We will reply shortly</p>
         <p>
           <Link to="/">Back to home page</Link>
         </p>
-      </Container>
+      </div>
     )
   }
 }
