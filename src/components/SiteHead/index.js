ebd62776668badeb0a593f94c425ce92921801a9
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-05-12 20:05:41 +0200

    shrink site head font size to 3 vw

--- src/components/SiteHead/index.js
+++ src/components/SiteHead/index.js
@@ -7,7 +7,7 @@ class SiteHeader extends React.PureComponent {
     return (
       <Header as="h3" icon textAlign="center" style={{ marginBottom: '2em' }}>
         <Header.Content style={{ width: '100%', margin: '0 auto' }}>
-          <p style={{ fontSize: '4vw', letterSpacing: '0.3em' }}>
+          <p style={{ fontSize: '3vw', letterSpacing: '0.3em' }}>
             I <Image style={{ display: 'inline', height: '1em', verticalAlign: 'text-bottom' }} src={logo} alt={'logo'} /> HANDMADE
           </p>
         </Header.Content>
